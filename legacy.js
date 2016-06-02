e6f963cb78a452a305199d16d14950619fdef55b
Salim KAYABAŞI <salim.kayabasi@gmail.com>
2016-06-02 14:17:14 +0300

    adding more `env`s

--- legacy.js
+++ legacy.js
@@ -3,9 +3,26 @@
 module.exports = {
   extends: 'airbnb/legacy',
   plugins: ['json'],
+  evn: {
+    mocha: true,
+    browser: true,
+    jquery: true,
+    mongo: true,
+    node: true
+  },
   rules: {
     'func-names': 'off',
     'vars-on-top': 'off',
-    'max-len': [2, 100, 2, { ignoreUrls: true }]
+    'max-len': [2, 100, 2, { ignoreUrls: true }],
+    'no-underscore-dangle': [
+      'error',
+      {
+        allow: [
+          '_id'
+        ]
+      }
+    ],
+    'no-param-reassign': 'off',
+    'global-require': 'off'
   }
 };
